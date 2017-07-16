15335f07ab7c8532ec6d44bca0e38b8b5d6e96cf
rado <rkajy@hotmail.fr>
2017-07-16 21:05:09 +0200

    réinitialisation initialisation de database.php

--- app/database.php
+++ app/database.php
@@ -9,7 +9,7 @@ $capsule->addConnection([
 	'host' => '127.0.0.1',
 	'username' => 'root',
 	'password' => 'toor',
-	'database' => 'website',
+	'database' => 'ARME_BD',
 	'charset' => 'utf8',
 	'collation' => 'utf8_unicode_ci',
 	'prefix' => ''
