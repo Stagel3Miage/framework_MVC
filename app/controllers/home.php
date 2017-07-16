345e6b7a4fcd67459657a4e5999d980bbeea37fa
rado <rkajy@hotmail.fr>
2017-07-16 18:48:40 +0200

    debut migration site web de arme en mvc

---
+++ app/controllers/home.php
@@ -0,0 +1,16 @@
+<?php
+
+class Home extends Controller
+{
+  public function index($name = '')
+  {
+      $this->view('home/index', ['name' => $user->name]);
+  }
+
+  public function create($username ='', $email = ''){
+  			User::create([
+  		    	'username' => $username,
+      			'email' => $email
+  			]);
+  }
+}
---
+++ app/views/forms/mdp_oublie.php
@@ -0,0 +1,22 @@
+<div id="mdpoublieModal" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"
+          aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Mot de passe perdu</h4>
+      </div>
+      <div class="modal-body">
+        <form action = "xxxx.php" method="POST" class="form-horizontal">
+            <p>Veuillez saisir votre adresse email</p>
+            <input type="text" class="form-control" id="email_mdp_oublie" name ="email_mdp_oublie" placeholder="Email" aria-hidden="true">
+            <div class="modal-footer">
+              <input type="submit" class="btn btn-primary" name="submit" value="Valider"/>
+              <button type="button" type="submit" class="btn btn-default btn--sm" data-dismiss="modal">Quitter</button>
+            </div>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</div>
